5ac5996d1e1e136f17c1e169b3520e0ce3691f0b
BarmanTurbo <21035407@student.hhs.nl>
2023-01-26 16:15:04 +0100

    test 1

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -3,3 +3,12 @@ describe('Cypress test nummmer 1', () => {
     expect(true).to.equal(true)
   })
 })
+
+describe('Test account aanmaken: offscreen elements should not exist',()=>{
+  it('visits the website', () => {
+    cy.visit('http://localhost:3000').get()
+    cy.get('ul>li').contains('Mijn account').click()
+    cy.get('section>p>a').contains('Klik dan hier om een account aan te maken').click()
+    cy.get('.offscreen').should('not.exist');
+  })
+})
