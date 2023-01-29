5ea524b8a3be53c85fba6a0a801479ec5a2b2623
BarmanTurbo <21035407@student.hhs.nl>
2023-01-29 22:51:53 +0100

    random accessibility improvement tussendoor

--- frontend/src/Pages/Shared/Header.js
+++ frontend/src/Pages/Shared/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { MenuItem } from "react-bootstrap";
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,14 +13,14 @@ const Header = () => {
                 }
                 ><i className="fa fa-bars"></i></button>
                 <div className={isMenuOpen ? "menu open" : "menu"}>
-                    <ul>
+                    <ul role="menubar" aria-label="Menu balk">
                         <li><a href="/">Home</a></li>
-                        <li><a href="/Programma">Programma</a></li>
-                        <li><a href="/OverOns">Over ons</a></li>
-                        <li><a href="/Huren">Huur</a></li>
-                        <li><a href="/Account">Mijn account</a></li>
-                        <li><a href="/Winkelmand"><i className="fa-solid fa-basket-shopping"></i></a></li>
-                        <li><a href="/Accesibility"><i className="fa fa-wheelchair" /></a></li>
+                        <li ><a href="/Programma" role="menuitem">Programma</a></li>
+                        <li><a href="/OverOns" role="menuitem">Over ons</a></li>
+                        <li><a href="/Huren" role="menuitem">Huur</a></li>
+                        <li><a href="/Account" role="menuitem">Mijn account</a></li>
+                        <li><a href="/Winkelmand" role="menuitem"><i className="fa-solid fa-basket-shopping"></i></a></li>
+                        <li><a href="/Accesibility" role="menuitem"><i className="fa fa-wheelchair" /></a></li>
                     </ul>
                 </div>
             </nav>
