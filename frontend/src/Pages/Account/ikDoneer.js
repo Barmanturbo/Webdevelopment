05e186ed10d5ca487f15637a91b96717b751465c
BarmanTurbo <21035407@student.hhs.nl>
2023-01-30 11:50:33 +0100

    Critical bug fix (4)

--- frontend/src/Pages/Account/ikDoneer.js
+++ frontend/src/Pages/Account/ikDoneer.js
@@ -1,7 +1,7 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Alinea from '../Shared/Alinea';
 
-const ikDoneer = (props) => {
+const IkDoneer = (props) => {
 
     var details = {
         "amount": props.bedrag,
@@ -24,10 +24,11 @@ const ikDoneer = (props) => {
         fetch('https://ikdoneer.azurewebsites.net/', {
             method: 'POST',
             headers:{
-            Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMWNjM2RjYi1kZmU0LTQ2Y2QtYjBmNC04MmZlMzlhNWE2ZjAiLCJqdGkiOiIzZjg0ZmIzOS02N2RmLTRkMWEtYjk4OS02MTkyMWNkMzQ2YTYiLCJpYXQiOiIwMS8yOS8yMDIzIDIxOjA2OjM3IiwiVXNlcklkIjoiMzFjYzNkY2ItZGZlNC00NmNkLWIwZjQtODJmZTM5YTVhNmYwIiwiRW1haWwiOiIyMTAzNTQwN0BzdHVkZW50Lmhocy5ubCIsImV4cCI6MTk5MDY0NTU5NywiaXNzIjoiSWtEb25lZXIiLCJhdWQiOiIqIn0.vu-4QGq2GgPC-l9Ib-DVKpOq69dwDVSQM6Co8qOCVa0',
-            'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'Access-Control-Allow-Origin': '*'},    
+                Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMWNjM2RjYi1kZmU0LTQ2Y2QtYjBmNC04MmZlMzlhNWE2ZjAiLCJqdGkiOiIzZjg0ZmIzOS02N2RmLTRkMWEtYjk4OS02MTkyMWNkMzQ2YTYiLCJpYXQiOiIwMS8yOS8yMDIzIDIxOjA2OjM3IiwiVXNlcklkIjoiMzFjYzNkY2ItZGZlNC00NmNkLWIwZjQtODJmZTM5YTVhNmYwIiwiRW1haWwiOiIyMTAzNTQwN0BzdHVkZW50Lmhocy5ubCIsImV4cCI6MTk5MDY0NTU5NywiaXNzIjoiSWtEb25lZXIiLCJhdWQiOiIqIn0.vu-4QGq2GgPC-l9Ib-DVKpOq69dwDVSQM6Co8qOCVa0',
+                'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*'
+            },    
             body: formBody
         })
             .then(response => {
@@ -54,4 +55,4 @@ const ikDoneer = (props) => {
     }
 }
 
-export default ikDoneer;
+export default IkDoneer;
