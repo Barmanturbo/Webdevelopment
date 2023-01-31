a955921e48dd62fc9550ffc69448ca54e82c24be
BarmanTurbo <21035407@student.hhs.nl>
2023-01-31 12:51:57 +0100

    admin done?

--- frontend/src/Pages/Account/Admin.js
+++ frontend/src/Pages/Account/Admin.js
@@ -6,48 +6,97 @@ const ZaalToevoegen = () => {
     const [addZaal, setAddZaal] = useState(0);
 
     const [roomname, setRoomname] = useState("");
-    const [roomnumber, setRoomnumber] = useState("");
+    const [roomnumber, setRoomnumber] = useState(0);
     const [capaciteit,setCapaciteit] = useState("");
+    
+    const [elementID, setElementID] = useState(0)
 
     const [rangnummer, setRangnummer] = useState(0);
     const [rijZaal, setRijZaal] = useState(0);
     const [rijCapaciteit, setRijCapaciteit] = useState(0);
-    const handleSubmit=async()=>{
-        try{
 
+    const [success, setSuccess]= useState(false);
+
+    const clearValues = ()=>{
+        setRoomname("");
+        setAddZaal(0);
+        setRoomnumber(0);
+        setCapaciteit("");
+        setElementID(0);
+        setRangnummer(0);
+        setRijZaal(0);
+        setRijCapaciteit(0);
+        setSuccess(false);
+    }
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+
+        try{
+            let res;
+            if(addZaal===1){
+                res = await fetch("https://localhost:7214/api/Zaal",{
+                    method: "POST",
+                    headers: {
+                        "Content-Type":"application/json",
+                    },
+                    body: JSON.stringify({
+                        Zaalnr: roomnumber,
+                        Naam: roomname,
+                        Aantal_stoelen: capaciteit
+                    }),
+                })
+            }else{
+                if(addZaal===2){
+                    res = await fetch("https://localhost:7214/api/Ruimte",{
+                        method: "POST",
+                        headers: {
+                            "Content-Type":"application/json",
+                        },
+                        body: JSON.stringify({
+                            RuimteNR: roomnumber,
+                            Naam: roomname,
+                            Capaciteit: capaciteit
+                        }),
+                    })
+                }else{
+                    if(addZaal===3){
+                        res = await fetch("https://localhost:7214/api/Stoelrij",{
+                        method: "POST",
+                        headers: {
+                            "Content-Type":"application/json",
+                        },
+                        body: JSON.stringify({
+                            rijid: elementID,
+                            Rangnummer: rangnummer,
+                            Aantal_stoelen: rijCapaciteit,
+                            Zaalnr: rijZaal
+                        }),
+                    })
+                    }else{
+                        throw "Invalid option. Choose a valid option"
+                    }
+                }
+            }
+            if(res.status===200){
+                setSuccess(true);
+                console.log("success")
+            }
         }catch(err){
             console.log(err);
         }
     }
 
-    /*const handleSubmit = async () => {
-        try {
-            let res = await fetch("https://localhost:7214/api/Show", {
-                method: "POST",
-                headers: {
-                "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                shownr: 0,
-                afbeelding: "string",
-                genre: showgenre,
-                naam: shownaam,
-                leeftijdsgroep: leeftijd,
-                zaal: showzaal,
-                beginTijd: "2023-01-22T22:09:07.168Z",
-                eindTijd: "2023-01-22T22:09:07.168Z"
-                }),
-            });
-          if (res.status === 200) {
-            console.log("succes");
-          }
-        } catch (err) {
-          console.log(err);
-        }
-    }*/
 
     return (
-        <>
+        <>{success?(
+            <>
+            <Hero2 tekst="Zalen configureren"/>
+            <Alinea teksts="Zaal met succes toegevoegd."/>
+            <button className="btn" onClick={(e)=>clearValues()}>Nog een zaal toevoegen?</button>
+            </>
+        ):(
+            <>
             <Hero2 tekst="Zalen configureren" />
             <section className="contact">
                 <h3>Type</h3>
@@ -93,6 +142,9 @@ const ZaalToevoegen = () => {
                                     <label htmlfor="ruimtenaam">Ruimtenaam:</label><br />
                                     <input type="text" id="ruimtenaam" autoComplete="off" onChange={(e) => setRoomname(e.target.value)} required />
                                     <br />
+                                    <label htmlfor="ruimteid">Ruimte nummer:</label><br/>
+                                    <input type="number" id="ruimteid" autoComplete="off" onChange={(e) => setRoomnumber(e.target.value)} required />
+                                    <br/>
                                     <label htmlfor="capaciteit">Capaciteit:</label><br />
                                     <input type="number" id="capaciteit" autoComplete="off" onChange={(e) => setCapaciteit(e.target.value)} required />
                                     <button className="btn">Ruimte toevoegen</button>
@@ -105,18 +157,22 @@ const ZaalToevoegen = () => {
                                     <label htmlfor="rang">Rangnummer:</label><br/>
                                     <input type="number" id="rang" autoComplete="off" onChange={(e)=>setRangnummer(e.target.value)} required/>
                                     <br/>
-                                    <label htmlFor="zaalnummer">Zaalnummer:</label>
+                                    <label htmlFor="zaalnummer">Zaalnummer:</label><br/>
                                     <input type="number" id="zaalnummer" autoComplete="off" onChange={(e)=>setRijZaal(e.target.value)} required/>
                                     <br/>
-                                    <label htmlFor="rijcapaciteit">Aantal stoelen:</label>
+                                    <label htmlFor="rijcapaciteit">Aantal stoelen:</label><br/>
                                     <input type="number" id="rijcapaciteit" autoComplete="off" onChange={(e)=>setRijCapaciteit(e.target.value)} required/>
+                                    <br/>
+                                    <label htmlfor="rijid">Rij ID:</label><br />
+                                    <input type="number" id="rijid" autoComplete="off" onChange={(e) => setElementID(e.target.value)} required />
                                     <button className="btn">Rij toevoegen</button>
                                 </form>
                             </>
                         )}
                     </>
                 )}
-            </section>   
+            </section>
+            </>)}   
         </>
     );
 }
